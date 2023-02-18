2a43a6effbd9e748d2bb5b7c3d065166221bd161
Jamescog <jamescog72@gmail.com>
2023-02-18 20:39:48 +0300

    pep8 complient

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -17,10 +17,8 @@ class Server:
 
     DATA_FILE = "Popular_Baby_Names.csv"
 
-
     def __init__(self):
         self.__dataset = None
-
 
     def index_range(self, page, page_size):
         """
@@ -29,7 +27,6 @@ class Server:
             a list for the those particular pagination parameters
         """
         return ((page * page_size - page_size), (page * page_size))
-
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -42,7 +39,6 @@ class Server:
                 self.__dataset = dataset[1:]
         return self.__dataset
 
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """a function that implements  pagination on huge dataset
         """
@@ -50,12 +46,9 @@ class Server:
         condtion1 = type(page) == int and page > 0
         condtion2 = type(page_size) == int and page_size > 0
 
-        if not(condtion1 and condtion2):
+        if not (condtion1 and condtion2):
             raise AssertionError
 
         index = self.index_range(page, page_size)
 
         return self.dataset()[(index[0]):(index[1])]
-
-
-    
