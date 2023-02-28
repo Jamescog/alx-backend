f9f95929ebce9fca6d103ee47a10b2c3d3054d9a
Jamescog <jamescog72@gmail.com>
2023-02-28 09:30:37 +0300

    Task 0 completed

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+"""0-Basic Flask app"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """renders index.html
+    """
+
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
